4624833f9ee55de3567d1881c22d029bdff00ad0
saibottrenham <toby.mahnert@rocketboots.com>
2021-08-23 22:25:04 +1000

    removed unneeded folders

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -13,7 +13,6 @@ import { AppRoutingModule } from './app-routing.module';
 import { HeaderComponent } from './navigation/header/header.component';
 import { SidenavListComponent } from './navigation/sidenav-list/sidenav-list.component';
 import { AuthService } from './auth/auth.service';
-import { TrainingService } from './training/training.service';
 import { environment } from '../environments/environment';
 import { UiService } from './shared/ui.service';
 import { AuthModule } from './auth/auth.module';
@@ -45,7 +44,7 @@ import { PropertyManagerService } from './property-manager/property-manager.serv
     StoreModule.forRoot(reducers),
     AuthModule
   ],
-  providers: [AuthService, TrainingService, UiService, PropertyManagerService],
+  providers: [AuthService, UiService, PropertyManagerService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
